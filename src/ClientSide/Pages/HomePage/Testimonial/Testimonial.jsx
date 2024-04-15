2f3996fb93ea5bf752d1e2c47522a0a0bd502517
SaddamHossainRepo <saddam1502064@gmail.com>
2024-04-15 20:54:49 +0600

    testimonial partial done

--- src/ClientSide/Pages/HomePage/Testimonial/Testimonial.jsx
+++ src/ClientSide/Pages/HomePage/Testimonial/Testimonial.jsx
@@ -3,6 +3,8 @@ import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
 import './testimonial.css';
 import profile from "/profile.png"
+import pic1 from "/pic-1.png"
+import pic2 from "/pic-2.png"
 import line from "/line.png"
 import { FaArrowRightLong } from "react-icons/fa6";
 import { FaArrowLeftLong } from "react-icons/fa6";
@@ -61,7 +63,7 @@ const Testimonial = () => {
                                     />
                                 </div>
                                 <div className='text-white 2xl:ml-[20px] xl:ml-[15px] ml-[5px]'>
-                                    <h3 className='testHeading'>Nazmul Islam</h3>
+                                    <h3 className='testHeading'>Hmd S</h3>
                                     <div className='flex items-center'>
                                         <h4 className='testSubHeading'>Graphics Designer</h4>
                                         <h3 className='2xl:ml-[10px] 2xl:mt-[10px] xl:ml-[5px] xl:mt-[5px] ml-1 mt-1'><img className='h-[1px] xl:w-[25px] 2xl:w-[34px] w-[10px]' src={line} alt="" /></h3>
@@ -86,13 +88,13 @@ const Testimonial = () => {
                             <div className='flex items-center '>
                                 <div>
                                     <img
-                                        src={profile}
+                                        src={pic1}
                                         className="block 2xl:w-[51px] 2xl:h-[51px] xl:w-[42px] xl:h-[42px] w-[30px]"
                                         alt="Wild Landscape"
                                     />
                                 </div>
                                 <div className='text-white 2xl:ml-[20px] xl:ml-[15px] ml-[5px]'>
-                                    <h3 className='testHeading'>Nazmul Islam</h3>
+                                    <h3 className='testHeading'>Liban Yasin</h3>
                                     <div className='flex items-center'>
                                         <h4 className='testSubHeading'>Graphics Designer</h4>
                                         <h3 className='2xl:ml-[10px] 2xl:mt-[10px] xl:ml-[5px] xl:mt-[5px] ml-1 mt-1'><img className='h-[1px] xl:w-[25px] 2xl:w-[34px] w-[10px]' src={line} alt="" /></h3>
@@ -105,7 +107,7 @@ const Testimonial = () => {
                             xl:text-[18px] xl:leading-[25px] xl:mt-0
                             text-[14px] mt-[20px] 
                             font-glacialBase  xl:bg-[url('/testimonialBg.png')] 2xl:bg-[url('/testimonialBg.png')] bg-[url('/testimonialBgMobile.png')]   bg-no-repeat bg-left-top">
-                               Working with”Fowzi Media” has been an absolute pleasure! Their talent and expertise in logo design and graphics have truly elevated my brand as a photographer and videographer. They have a keen eye for detail and always deliver designs that perfectly capture the essence of my work. Not only are they incredibly talented, but they are also a reliable and efficient collaborator. They take the time to understand my vision and turn it into reality, exceeding my expectations every time. I highly recommend “Fowzi Media” for anyone in need of logo design, graphics, and branding services. Their exceptional work will undoubtedly make a positive impact on your brand identity.
+                               They provided me with a high quality work for a friendly price and responded to feedback in a timely way. I will highly recommend to using Fowzi Media's services. A very satisfied client. Thanks
                             </p>
                             {/* <p className="pt-[5px] text-[#FF0101]" onClick={()=>showBtn()}>{show? "Show More" : "Show Less"}</p> */}
                         </div>
@@ -117,7 +119,7 @@ const Testimonial = () => {
                             <div className='flex items-center '>
                                 <div>
                                     <img
-                                        src={profile}
+                                        src={pic2}
                                         className="block 2xl:w-[51px] 2xl:h-[51px] xl:w-[42px] xl:h-[42px] w-[30px]"
                                         alt="Wild Landscape"
                                     />
